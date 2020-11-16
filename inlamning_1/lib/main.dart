0743c1cfcc8f27d11b25bfe96148c8f55f052e06
leolon <guslonnrle@student.gu.se>
2020-11-16 15:17:54 +0100

    Fixat till lite.

--- inlamning_1/lib/main.dart
+++ inlamning_1/lib/main.dart
@@ -6,7 +6,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MainView());
+    return MaterialApp(
+      home: MainView(),
+      );
   }
 }
 
@@ -21,13 +23,59 @@ class MainView extends StatelessWidget {
         backgroundColor: Colors.grey[400],
         actions: [
           IconButton(
-            icon: Icon(Icons.arrow_right),
+            icon: Icon(Icons.more_vert,
+            color: Colors.black,
+            ),
             onPressed: () {
               Navigator.push(context,
                   MaterialPageRoute(builder: (context) => SecondView()));
             },
           ),
         ],
+      ),
+      body: _list(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add, size: 56),
+        backgroundColor: Colors.grey,
+      ),
+    );
+  }
+    Widget _list() {
+    var todolist = ['Write a book', 'Do homework', 'Tidy room', 'Watch TV','Nap', 'Shop groceries', 'Have fun', 'Meditate'];
+    var newlist = List.generate(100,(index) =>'${todolist[index % 8]}');
+    return ListView.builder(
+      itemBuilder: (context, index) => _item(newlist[index]),
+      itemCount: 8,
+        
+    );
+  }
+
+   Widget _item(text) {
+    return CheckboxListTile(
+      title: Text(text,
+          style: TextStyle(
+            fontSize: 26,
+          )),
+      secondary: Icon(Icons.cancel),
+      value: false,
+      onChanged: (val) {},
+      controlAffinity: ListTileControlAffinity.leading,
+    );
+  }
+}
+  
+
+
+
+class SecondView extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('TIG169 TODO',
+         style: TextStyle(color: Colors.black, fontSize: 26), ),
+        backgroundColor: Colors.grey[400],
       ),
       body: Center(
         child: Column(
@@ -57,43 +105,6 @@ class MainView extends StatelessWidget {
     );
   }
 
+ 
 }
-
-class SecondView extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('TIG169 TODO',
-         style: TextStyle(color: Colors.black, fontSize: 26), ),
-        backgroundColor: Colors.grey[400],
-      ),
-     body:  _list(),
-    );
-  }
-
-  Widget _list() {
-    var list = ['Write a book', 'Do homework', 'Tidy room', 'Watch TV','Nap', 'Shop groceries', 'Have fun', 'Meditate'];
-    return ListView(
-      children: 
-        list.map((item) => _item(item)).toList(),
-        
-    );
-  }
-
-  Widget _item(text) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Text(text, style: TextStyle(fontSize: 26)),
-    );
-  }
-  Widget _checkBox() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Checkbox(value: false, onChanged: (val) {} )
-    ],
-    );
-  }
-}
+  
